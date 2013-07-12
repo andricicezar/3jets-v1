10d47c8a4326f80c3fd66bc6edf3cc52384cfd18
Andrici Cezar <cezaric96@yahoo.com>
2013-07-12 13:13:23 +0300

    flex design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :mobile_device
+  
+  def mobile_device
+    @mobile_device = false
+    if request.user_agent =~ /Mobile|webOS/
+      @mobile_device = true
+    end
+  end
 
   def current_user
     super || guest_user
